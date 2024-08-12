43227a68f42fb830083c073ce5954409aecd8edf
Maker424 <jimmydelgado0812@gmail.com>
2024-08-12 00:39:25 -0700

    Connected the backend to our mongoDB database, and prepared sanitization for login and registration page

--- core-marker-app/Backend/server.js
+++ core-marker-app/Backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require("cors");
+const mongoSanitize = require('express-mongo-sanitize');
+
+const mongoDB_ConnectionString = 'mongodb+srv://Maker424:CS179K-Project-Legend@core-marker-database.lsbe7.mongodb.net/?retryWrites=true&w=majority&appName=Core-Marker-Database';
+
+mongoose.connect(mongoDB_ConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to Core-Marker MongoDB'))
+    .catch(error => console.error('Could not connect to MongoDB:', error))
 
 const app = express();
+const port = 8080;
 
 app.use(cors());
 
-const mongoDB_ConnectionString = '';
+app.use(
+    mongoSanitize({
+      onSanitize: ({ req, key }) => {
+        console.warn(`This request[${key}] is sanitized`, req);
+      },
+    }),
+);
 
-mongoose.connect()
+// CSS stylesheet routes
+app.use(express.static(path.join(__dirname, "public")));
