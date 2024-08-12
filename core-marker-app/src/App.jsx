3832637e2948232a64a777764968ab24212d27cc
py1u.dev <py1u.dev@gmail.com>
2024-08-12 14:16:29 -0700

    test: static card examples for demo

--- core-marker-app/src/App.jsx
+++ core-marker-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import posts from "./data/posts";
+import Post from "./components/Post";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="p-8">
+      <Navbar />
 
-export default App
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {posts.map((card, index) => (
+          <Post
+            key={index}
+            title={card.title}
+            content={card.content}
+            image={card.image}
+          />
+        ))}
+      </div>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default App;
