5b68549a4b710d3da816341e494d1b2060a86117
py1u.dev <py1u.dev@gmail.com>
2024-08-19 10:01:55 -0700

    add auth session for home page and route component for homepage

--- core-marker-app/src/main.jsx
+++ core-marker-app/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
 import Root from "./routes/root";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 import ErrorPage from "./error-page";
+import Login from "./pages/Login";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,8 +15,29 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: "/Signup",
+    path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    loader: async () => {
+      const response = await fetch("/api/check-auth", {
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (!data.isAuthenticated) {
+        throw new Response(null, {
+          status: 302,
+          headers: { Location: "/login" },
+        });
+      }
+      return null;
+    },
   },
 ]);
 
