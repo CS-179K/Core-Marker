78ce77a05cddc97942cceb28ce17e69d99a8acbf
py1u.dev <py1u.dev@gmail.com>
2024-08-20 22:07:26 -0700

    fix: adjust src/components/Navbar.jsx logic

--- core-marker-app/src/components/Navbar.jsx
+++ core-marker-app/src/components/Navbar.jsx
@@ -4,26 +4,26 @@ import "../components/Navbar.css"; // Optional custom styles
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-semibold">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-semibold text-white">
           <Link to="/">Core-Marker</Link>
         </div>
         <div className="space-x-4">
           <Link
             to="/home"
-            className="text-white hover:text-gray-400 transition duration-300"
+            className="text-white transition duration-300 hover:text-gray-400"
           >
             Home
           </Link>
           <Link
             to="/post"
-            className="text-white hover:text-gray-400 transition duration-300"
+            className="text-white transition duration-300 hover:text-gray-400"
           >
             Post
           </Link>
           <Link
             to="/profile"
-            className="text-white hover:text-gray-400 transition duration-300"
+            className="text-white transition duration-300 hover:text-gray-400"
           >
             My Profile
           </Link>
