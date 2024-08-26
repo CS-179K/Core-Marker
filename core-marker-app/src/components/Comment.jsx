55d6c2700c1295c7f19e1be7bedeaa8e29314d9c
py1u.dev <py1u.dev@gmail.com>
2024-08-26 14:30:00 -0700

    refactor: src/components/Comment.jsx with prettier

--- core-marker-app/src/components/Comment.jsx
+++ core-marker-app/src/components/Comment.jsx
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Comment = ({ username, comment }) => {
   return (
-    <div className="border-t border-gray-200 mt-4 pt-4">
+    <div className="mt-4 border-t border-gray-200 pt-4">
       <p className="font-semibold">{username}</p>
       <p className="text-gray-600">{comment}</p>
     </div>
