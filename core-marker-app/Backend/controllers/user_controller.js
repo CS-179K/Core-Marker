ce2b4dc755bb94e2e022b1359fb93c05d7775332
py1u.dev <py1u.dev@gmail.com>
2024-08-26 14:05:25 -0700

    test: user controller on profile page

---
+++ core-marker-app/Backend/controllers/user_controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user_model.js";
+import jwt from "jsonwebtoken";
+
+export const getUser = async (req, res) => {
+  const token = req.headers["x-access-token"];
+  if (!token) {
+    console.log("No token provided");
+    return res
+      .status(401)
+      .json({ status: "error", message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    console.log("Token decoded:", decoded);
+    const userId = decoded.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      console.log("User not found");
+      return res
+        .status(404)
+        .json({ status: "error", message: "User not found" });
+    }
+    console.log("User found:", user);
+    res.json({ status: "ok", user });
+  } catch (error) {
+    console.log("Error verifying token or fetching user:", error.message);
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
