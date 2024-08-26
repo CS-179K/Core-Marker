c63eeae9b49144083dd19c8a7dfe8a4de14f4133
py1u.dev <py1u.dev@gmail.com>
2024-08-26 13:49:24 -0700

    update: post route with createPost route and get user post route

--- core-marker-app/Backend/routes/post_route.js
+++ core-marker-app/Backend/routes/post_route.js
@@ -4,19 +4,24 @@ import {
   createPost,
   updatePost,
   deletePost,
+  getPostsByUser,
 } from "../controllers/post_controller.js";
+
 const router = express.Router();
 
 // returns all posts
 router.get("/", getAllPosts);
 
 // creates a post
-router.post("/", createPost);
+router.post("/api/upload", createPost);
 
-//updates a post
+// updates a post
 router.put("/:id", updatePost);
 
 // deletes a post by id
 router.delete("/:id", deletePost);
+
+// returns posts by user
+router.get("/user/:userId", getPostsByUser);
 
 export default router;
