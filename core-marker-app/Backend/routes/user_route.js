2766a560099b7dbe2f7964cb6e705ed8cdb68955
py1u.dev <py1u.dev@gmail.com>
2024-09-01 21:19:23 -0700

    create: routes for api endpoints to update avatar and banner

--- core-marker-app/Backend/routes/user_route.js
+++ core-marker-app/Backend/routes/user_route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUser, getUserPosts } from "../controllers/user_controller.js";
+import { getUser, getUserPosts, updateBanner, updateAvatar } from "../controllers/user_controller.js";
 
 const router = express.Router();
 
 router.get("/me", getUser);
 router.get("/:userId", getUserPosts);
-
+router.post("/update/avatar", updateAvatar);
+router.post("/update/banner", updateBanner);
 export default router;
